e324417adee201fee852e77c1d6df88971249673
Sujal Verma <sujalverma@Sujals-MacBook-Pro.local>
2025-05-22 01:23:53 +0530

    day 2

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
-fn main() {
-    let name = String::from("sujal verma");
-    let first = get_first_name(name);
-    println!("first name : {}", first);
-}
 
-fn get_first_name(name: String) -> String {
-    let mut ans = String::from("");
-    for ch in name.chars() {
-        if ch == ' ' {
-            break;
-        }
-        ans.push(ch);
+
+fn main() {
+    let num: i32 = 100;
+    let target: i32 = 36;
+    let found : bool= find_it(num, target);
+    if found {
+        println!("Number found");
     }
-    return ans;
+    else{
+        println!("Number not found");
+    }
+}
+// boinary search.
+
+fn find_it(num: i32, target: i32) -> bool {
+    let mut s: i32 = 0;
+    let mut e: i32 = num;
+    // let mut m: i32 = s + (e - s) / 2;
+    while s <= e {
+          let m = s + (e - s) / 2;
+        if target == m {
+            return true;
+        } else {
+            if target > m {
+                s = m + 1;
+            } else {
+                e = m - 1;
+            }
+        }
+       
+    }
+
+    return false;
 }
