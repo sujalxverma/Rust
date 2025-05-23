ca855804b9548f15764436112d9e0ca5af117916
Sujal Verma <sujalverma@Sujals-MacBook-Pro.local>
2025-05-23 21:51:07 +0530

    memeory

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,8 @@
 
 
 fn main() {
-    let num: i32 = 100;
-    let target: i32 = 36;
-    let found : bool= find_it(num, target);
-    if found {
-        println!("Number found");
-    }
-    else{
-        println!("Number not found");
-    }
+    let mut name = String :: from("hello");
+    name.push_str("string");
+    println!("{}",name);
 }
-// boinary search.
 
-fn find_it(num: i32, target: i32) -> bool {
-    let mut s: i32 = 0;
-    let mut e: i32 = num;
-    // let mut m: i32 = s + (e - s) / 2;
-    while s <= e {
-          let m = s + (e - s) / 2;
-        if target == m {
-            return true;
-        } else {
-            if target > m {
-                s = m + 1;
-            } else {
-                e = m - 1;
-            }
-        }
-       
-    }
-
-    return false;
-}
