d0ba5ad556712bd994905939bc589871b7b55b84
Sujal Verma <sujalverma@Sujals-MacBook-Pro.local>
2025-05-24 07:51:45 +0530

    day4 morning

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+use std::io;
 
+//      take number as input
+//     let mut number = String::new();
+//     println!("Enter the number : ");
+//     io::stdin().read_line(&mut number).expect("Failed");
+//     let num :i32 = number.trim().parse().expect("Enter the number");
 
 fn main() {
-    let mut name = String :: from("hello");
-    name.push_str("string");
-    println!("{}",name);
-}
+   
 
+
+}
